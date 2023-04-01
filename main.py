7d4f46bdf7084d646008bf935d5656cf36591317
Kiril Kobzar <kirilkobzar@gmail.com>
2023-04-01 08:33:20 +0000

    final

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
+import time
 class Person:
   age = 22
   height = 175.5
   isMale = True
+  money = 0
   name = 'Kyryl'
 
   def __init__(self, name, age):
     self.name = name
     self.age = age
 
-me = Person()
-friend = Person()
 
-print(me.age)
-friend.name = "Kolya"
-friend.age = 15
-print(friend.name)
+  def work(self, days):
+    while days != 0:
+      time.sleep(1)
+      self.money += 2
+      days -= 1
+
+  
+  def rest(self, days):
+    while days != 0:
+      time.sleep(1)
+      self.money -= 1
+      days -= 1
+
+me = Person('Kyryl', 22)
+friend = Person('Vasyl', 20)
+
+print(me.money)
+me.work(3)
+print("Ви заробили", me.money, "money")
+
+
+# print(me.age)
+# print(friend.age)
+
+
+# print(me.age)
+# friend.name = "Kolya"
+# friend.age = 15
+# print(friend.name)
